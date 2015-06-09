f607143acdb549290793fa981c8df058d6277f44
neffnet <neff@neffnet.net>
2015-06-09 17:40:56 -0500

    create from the cli

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,9 +1,18 @@
 class ApiController < ApplicationController
   skip_before_action :verify_authenticity_token
+  before_action :verify_params
+
+  def verify_params
+    param_name = self.class.to_s.titleize.split.first.split('/')[1].singularize.downcase
+    if !params.key? param_name.to_sym
+      render json: {errors: "parameter missing: #{param_name}"}, status: :unprocessable_entity and return
+    end
+  end
 
   private
 
   def authenticated?
-    authenticate_or_request_with_http_basic { |username, password| User.where(name: username, password: password).present? }
+    authenticate_or_request_with_http_basic { |username, password| @current_user = User.where(name: username, password: password).first } 
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
+
     resources :users
+
+    resources :lists do
+      resources :items, only: [:create]
+    end
+
+    resources :items, only: [:destroy]
   end
 
   get 'welcome/index'
